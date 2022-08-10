878861a5f79912117e37e5b66024e58c702e25ff
donghwikkim <donghwi9507@gmail.com>
2022-08-10 15:14:35 +0900

    아이콘 정렬 및 수정/framelayout 부분 구현

--- app/src/main/java/com/example/risingcampw1/MainActivity.kt
+++ app/src/main/java/com/example/risingcampw1/MainActivity.kt
@@ -2,10 +2,29 @@ package com.example.risingcampw1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.ImageView
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setFrameLayoutRadius()
+    }
+
+    private fun setFrameLayoutRadius() {
+        iv_food1.clipToOutline = true
+        iv_food2.clipToOutline = true
+        iv_food3.clipToOutline = true
+        iv_food4.clipToOutline = true
+        iv_food5.clipToOutline = true
+        iv_food6.clipToOutline = true
+        iv_food7.clipToOutline = true
+        iv_food8.clipToOutline = true
+        fr_sale.clipToOutline = true
+        fr_more.clipToOutline = true
     }
 }
